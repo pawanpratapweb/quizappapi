6afac142d3572d6683f3700f61c072309e7a1802
pawanpratapweb <pawanpratap2008@gmail.com>
2021-12-26 08:43:17 +0000

    important

--- index.js
+++ index.js
@@ -9,25 +9,18 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-app.get("/api", (req, res) => {
-	if(req.query.psi == process.env.HOMEURL) {
-		res.setHeader('Access-Control-Allow-Origin', '*');
-		res.sendFile(__dirname + "/api.json");	
-	} else { res.send("parsing data from machine...") }
-})
-
 app.get("/api/random", (req, res) => {
-	if(req.query.trd == process.env.RANDURL) {
-		axios.get('https://quizappapis.herokuapp.com/api?psi=' + process.env.HOMEURL)
-		.then(response => {
-			function getRandomInt(min, max) {
-			min = Math.ceil(min);
-			max = Math.floor(max);
-			return Math.floor(Math.random() * (max - min + 1)) + min;
-			}
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.send(response.data[getRandomInt(0, response.data.length)])
-		}).catch(err => {console.log(err)})
+	if (req.query.trd == process.env.RANDURL) {
+		axios.get('https://quizzoweb-default-rtdb.firebaseio.com/gs.json')
+			.then(response => {
+				function getRandomInt(min, max) {
+					min = Math.ceil(min);
+					max = Math.floor(max);
+					return Math.floor(Math.random() * (max - min + 1)) + min;
+				}
+				res.setHeader('Access-Control-Allow-Origin', '*');
+				res.send(Object.values(response.data)[ getRandomInt(0, Object.values(response.data).length) ])
+			}).catch(err => { console.log(err) })
 	} else { res.send("parsing data from machine...") }
 
 })
--- public/quiz/script.js
+++ public/quiz/script.js
@@ -1,36 +1,36 @@
 document.querySelector(".points").innerText = localStorage.getItem('points');
 fetch(window.location.origin + "/api/random?trd=kjdh8sa6dsKJHKJKHJKH3897B9823490209")
-.then(res => {
-  return res.json()
-}).then(res => {
-  const question = document.querySelector(".question");
-  const options = document.getElementsByClassName("options")[0];
-  var answer = res.answer;
-  question.innerText = res.question;
-  res.options.map((elem, index) => {
-    options.insertAdjacentHTML("beforeend", `<button class="optionInd">${elem}</button>`);
-  })
-  const optionInd = document.getElementsByClassName("optionInd");
+  .then(res => {
+    return res.json()
+  }).then(res => {
+    const question = document.querySelector(".question");
+    const options = document.getElementsByClassName("options")[ 0 ];
+    var answer = res.answer;
+    question.innerText = res.question;
+    res.options.map((elem, index) => {
+      options.insertAdjacentHTML("beforeend", `<button class="optionInd">${elem}</button>`);
+    })
+    const optionInd = document.getElementsByClassName("optionInd");
 
-  for(var i=0; i<optionInd.length; i++){
-    optionInd[i].addEventListener("click", checkAnswer);
-  }
-  window.answer = res.answer.trim();
-}).catch(err => {
-  console.log(err)
-})
+    for (var i = 0; i < optionInd.length; i++) {
+      optionInd[ i ].addEventListener("click", checkAnswer);
+    }
+    window.answer = res.answer.trim();
+  }).catch(err => {
+    console.log(err)
+  })
 
 
 const checkAnswer = (event) => {
-  const banner  = document.querySelector(".banner");
+  const banner = document.querySelector(".banner");
   banner.style.display = "block";
   const element = event.target;
   element.style.background = "cyan";
   element.style.color = "black";
   element.style.fontWeight = "500";
-  
+
   setTimeout(() => {
-    if(element.innerHTML == answer){
+    if (element.innerHTML == answer) {
       const correctAudio = document.querySelector(".correctAudio");
       correctAudio.play();
       element.style.background = "#00ff40";
@@ -40,7 +40,7 @@ const checkAnswer = (event) => {
       element.style.background = "#ff4533";
       element.style.borderColor = "#ff4533";
       element.parentElement.childNodes.forEach((elem) => {
-        if(elem.innerText == answer){
+        if (elem.innerText == answer) {
           elem.style.background = "#00ff40";
           elem.style.fontWeight = "500";
           elem.style.color = "black";
